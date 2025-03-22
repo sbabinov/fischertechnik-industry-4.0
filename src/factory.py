bb810186ad9dd36fe1742d3d29baf088adc05855
sbabinov <serafimbabinov106@gmail.com>
2025-03-22 23:19:51 +0300

    feat: add classes architecture

---
+++ src/factory.py
@@ -0,0 +1,15 @@
+from stages import *
+
+class Factory:
+    """ Class for controlling the Fischertechnik factory layout. """
+
+    def __init__(self):
+        self.storage = Storage()
+        self.crane = Crane()
+        self.paintCenter = PaintingCenter()
+        self.shipmentCenter = ShipmentCenter()
+        self.sortingCenter = SortingCenter()
+
+    def calibrate(self) -> None:
+        """ Calibrates all components. """
+        ...
