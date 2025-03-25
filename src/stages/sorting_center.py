0e4bbda2bc87dba7f75da913377e0cf53cfe741d
Dmitriy <Dmitry5335@yandex.ru>
2025-03-25 22:19:47 +0300

    docs: add documentation to methods of SortingCenter

--- src/stages/sorting_center.py
+++ src/stages/sorting_center.py
@@ -8,22 +8,29 @@ class SortingCenter(Stage):
         self.__redCount = 0
 
     def sort(self) -> None:
+      """ Determine the color of the cargo and sort it """
       ...
 
     def decWhite(self) -> None:
+      """ Reduce the number of white goods """
       self.__whiteCount -= 1
 
     def decBlue(self) -> None:
+      """ Reduce the number of blue goods """
       self.__blueCount -= 1
 
     def decRed(self) -> None:
+      """ Reduce the number of red goods """
       self.__redCount -= 1
 
     def getWhite(self) -> int:
+      """ Return the number of white goods """
       return self.__whiteCount
 
     def getBlue(self) -> int:
+      """ Return the number of blue goods """
       return self.__blueCount
 
     def getRed(self) -> int:
+      """ Return the number of red goods """
       return self.__redCount
