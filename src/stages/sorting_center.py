f347de103e8852aa89d3d9721ff7ddf029429399
Dmitriy <Dmitry5335@yandex.ru>
2025-03-25 20:34:46 +0300

    feat: add sorting_center constructor

--- src/stages/sorting_center.py
+++ src/stages/sorting_center.py
@@ -1,4 +1,29 @@
 from .stage import Stage
 
 class SortingCenter(Stage):
-    ...
+    def __init__(self, host: str, port: int = 65000):
+        super().__init__(host, port)
+        self.__whiteCount = 0
+        self.__blueCount = 0
+        self.__redCount = 0
+
+    def sort(self) -> None:
+      ...
+
+    def decWhite(self) -> None:
+      ...
+
+    def decBlue(self) -> None:
+      ...
+
+    def decRed(self) -> None:
+      ...
+
+    def getWhite(self) -> int:
+      ...
+
+    def getBlue(self) -> int:
+      ...
+
+    def getRed(self) -> int:
+      ...
