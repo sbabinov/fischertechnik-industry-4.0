50763dac141bcb912ba4bf9fdc471e48f86b6604
Serafim Babinov <serafimbabinov106@gmail.com>
2025-04-02 20:36:03 +0300

    feat: add calibrate method and Motor wrapper

--- src/stages/crane.py
+++ src/stages/crane.py
@@ -1,27 +1,43 @@
 from .stage import Stage
 
+class Motor:
+    def __init__(self, stage, motor_id):
+        self.__stage = stage
+        self.__motor_id = motor_id
+        self.__motor = self.__stage.motor(motor_id)
+
+    def calibrate(self) -> None:
+        sensor = self.__stage.resistor(self.__motor_id)
+        while sensor.value() == 0:
+            sensor = self.__stage.resistor(self.__motor_id)
+        self.move(10000, wait=False)
+
+        while not self.isFinished():
+            if sensor.value() != 15000:
+                self.stop()
+                break
+            sensor = self.__stage.resistor(self.__motor_id)
+            self.__stage.updateWait()
+
+    def move(self, distance: int, speed: int = 512, wait: bool = True) -> None:
+        speed = -speed if distance < 0 else speed
+        self.__motor.setSpeed(speed)
+        self.__motor.setDistance(distance)
+        if wait:
+            self.__stage._wait(self.__motor)
+
+    def isFinished(self) -> bool:
+        return self.__motor.finished()
+
+    def stop(self) -> None:
+        self.__motor.stop()
+
 class Crane(Stage):
     def calibrate(self) -> None:
         """ Calibrates all crane motors. """
-        for i in 1, 2, 3:
-            sensor = self._stage.resistor(i)
-            while sensor.value() == 0:
-                sensor = self._stage.resistor(i)
-            motor = self._stage.motor(i)
-            motor.setSpeed(512)
-            motor.setDistance(10000)
-
-            while not motor.finished():
-                if sensor.value() != 15000:
-                    motor.stop()
-                    break
-                sensor = self._stage.resistor(i)
-                self._stage.updateWait()
-
-        motor = self._stage.motor(1)
-        motor.setSpeed(-512)
-        motor.setDistance(925)
-        self._stage.wait(motor)
+        for motor_id in 1, 2, 3:
+            Motor(self._stage, motor_id).calibrate()
+        Motor(self._stage, 1).move(-925)
 
     def takeFromStorage(self) -> None:
         motor1 = self._stage.motor(1)
