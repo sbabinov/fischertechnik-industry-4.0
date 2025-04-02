f42c0819b8f9c227f590d602c73ae9b5c0a07259
sbabinov <serafimbabinov106@gmail.com>
2025-04-02 22:13:54 +0300

    feat: new takeFromStorage() method

--- src/stages/crane.py
+++ src/stages/crane.py
@@ -9,27 +9,24 @@ class Crane(Stage):
         Motor(self._stage, 1).move(-925)
 
     def takeFromStorage(self) -> None:
-        motor1 = self._stage.motor(1)
-        motor1.setSpeed(-512)
-        motor1.setDistance(475)
-        self.wait(motor1)
+        distances = (-475, -250, -195)
 
-        motor3 = self._stage.motor(3)
-        motor3.setSpeed(-512)
-        motor3.setDistance(200)
-        self.wait(motor3)
+        tower = Motor(self._stage, 1)
+        arm_z = Motor(self._stage, 2)
+        arm_x = Motor(self._stage, 3)
+        generator = Motor(self._stage, 4)
 
-        motor2 = self._stage.motor(2)
-        motor2.setSpeed(-512)
-        motor2.setDistance(190)
-        self.wait(motor2)
-
-        motor4 = self._stage.motor(4)
-        motor4.setSpeed(512)
-        motor4.setDistance(1024)
+        tower.move(distances[0])
+        arm_x.move(distances[2])
+        arm_z.move(distances[1])
+        generator.move(wait=False)
         out = self._stage.output(8)
         out.setLevel(512)
-        self.wait(motor4)
+        time.sleep(2)
+
+        arm_z.move(-distances[1])
+        arm_x.move(-distances[2])
+        tower.move(-distances[0])
 
     def takeFromSortingCenter(self, color) -> None:
         if color == Cargo.WHITE:
