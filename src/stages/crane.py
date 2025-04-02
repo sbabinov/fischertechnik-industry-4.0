91377e88577c473932199878b9c97133cda1944d
sbabinov <serafimbabinov106@gmail.com>
2025-04-02 21:50:01 +0300

    refactor: delete Motor from crane.py

--- src/stages/crane.py
+++ src/stages/crane.py
@@ -1,37 +1,5 @@
 import time
-from .stage import Stage, Cargo
-
-class Motor:
-    def __init__(self, stage, motor_id):
-        self.__stage = stage
-        self.__motor_id = motor_id
-        self.__motor = self.__stage.motor(motor_id)
-
-    def calibrate(self) -> None:
-        sensor = self.__stage.resistor(self.__motor_id)
-        while sensor.value() == 0:
-            sensor = self.__stage.resistor(self.__motor_id)
-        self.move(10000, wait=False)
-
-        while not self.isFinished():
-            if sensor.value() != 15000:
-                self.stop()
-                break
-            sensor = self.__stage.resistor(self.__motor_id)
-            self.__stage.updateWait()
-
-    def move(self, distance: int = 100_000, speed: int = 512, wait: bool = True) -> None:
-        speed = -speed if distance < 0 else speed
-        self.__motor.setSpeed(speed)
-        self.__motor.setDistance(distance)
-        if wait:
-            self.__stage._wait(self.__motor)
-
-    def isFinished(self) -> bool:
-        return self.__motor.finished()
-
-    def stop(self) -> None:
-        self.__motor.stop()
+from .stage import Stage, Cargo, Motor
 
 class Crane(Stage):
     def calibrate(self) -> None:
@@ -92,7 +60,22 @@ class Crane(Stage):
         tower.move(-distances[0])
 
     def putInStorage(self) -> None:
-        ...
+        distances = (-475, -250, -195)
+
+        tower = Motor(self._stage, 1)
+        arm_z = Motor(self._stage, 2)
+        arm_x = Motor(self._stage, 3)
+        generator = Motor(self._stage, 4)
+
+        tower.move(distances[0])
+        arm_x.move(distances[2])
+        arm_z.move(distances[1])
+        generator.stop()
+
+        arm_z.move(-distances[1])
+        arm_x.move(-distances[2])
+        tower.move(-distances[0])
+
 
     def putInPaintingCenter(self) -> None:
         ...
