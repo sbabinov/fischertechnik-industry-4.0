b54c5c350678efc2149a533ac1dcdf10b6f1b2b3
sbabinov <serafimbabinov106@gmail.com>
2025-04-02 22:27:56 +0300

    refactor: remove code duplication

--- src/stages/crane.py
+++ src/stages/crane.py
@@ -1,7 +1,24 @@
 import time
+from typing import Tuple
 from .stage import Stage, Cargo, Motor
 
 class Crane(Stage):
+    def __init__(self, host: str, port: int = 65000):
+        super().__init__(host, port)
+        self.tower = Motor(self._stage, 1)
+        self.arm_z = Motor(self._stage, 2)
+        self.arm_x = Motor(self._stage, 3)
+
+    def __move(self, distances: Tuple) -> None:
+        self.tower.move(distances[0])
+        self.arm_x.move(distances[2])
+        self.arm_z.move(distances[1])
+
+    def __move_back(self, distances: Tuple) -> None:
+        self.arm_z.move(-distances[1])
+        self.arm_x.move(-distances[2])
+        self.tower.move(-distances[0])
+
     def calibrate(self) -> None:
         """ Calibrates all crane motors. """
         for motor_id in 1, 2, 3:
@@ -11,23 +28,14 @@ class Crane(Stage):
     def takeFromStorage(self) -> None:
         """ The crane takes a cargo from the storage and returns to the calibrated position. """
         distances = (-475, -250, -195)
-
-        tower = Motor(self._stage, 1)
-        arm_z = Motor(self._stage, 2)
-        arm_x = Motor(self._stage, 3)
         generator = Motor(self._stage, 4)
 
-        tower.move(distances[0])
-        arm_x.move(distances[2])
-        arm_z.move(distances[1])
+        self.__move(distances)
         generator.move(wait=False)
         out = self._stage.output(8)
         out.setLevel(512)
         time.sleep(2)
-
-        arm_z.move(-distances[1])
-        arm_x.move(-distances[2])
-        tower.move(-distances[0])
+        self.__move_back(distances)
 
     def takeFromSortingCenter(self, color) -> None:
         """ The crane takes a cargo from the sorting center and returns to the calibrated position. """
@@ -37,56 +45,29 @@ class Crane(Stage):
             distances = (545, -855, -420)
         else:
             distances = (615, -855, -600)
-
-        tower = Motor(self._stage, 1)
-        arm_z = Motor(self._stage, 2)
-        arm_x = Motor(self._stage, 3)
         generator = Motor(self._stage, 4)
 
-        tower.move(distances[0])
-        arm_x.move(distances[2])
-        arm_z.move(distances[1])
+        self.__move(distances)
         generator.move(wait=False)
         out = self._stage.output(8)
         out.setLevel(512)
         time.sleep(2)
-
-        arm_z.move(-distances[1])
-        arm_x.move(-distances[2])
-        tower.move(-distances[0])
+        self.__move_back(distances)
 
     def putInStorage(self) -> None:
         """ The crane puts a cargo in the storage and returns to the calibrated position. """
         distances = (-475, -250, -195)
-
-        tower = Motor(self._stage, 1)
-        arm_z = Motor(self._stage, 2)
-        arm_x = Motor(self._stage, 3)
         generator = Motor(self._stage, 4)
 
-        tower.move(distances[0])
-        arm_x.move(distances[2])
-        arm_z.move(distances[1])
+        self.__move(distances)
         generator.stop()
-
-        arm_z.move(-distances[1])
-        arm_x.move(-distances[2])
-        tower.move(-distances[0])
+        self.__move_back(distances)
 
     def putInPaintingCenter(self) -> None:
         """ The crane puts a cargo in the painting center and returns to the calibrated position. """
         distances = (5, -600, -835)
-
-        tower = Motor(self._stage, 1)
-        arm_z = Motor(self._stage, 2)
-        arm_x = Motor(self._stage, 3)
         generator = Motor(self._stage, 4)
 
-        tower.move(distances[0])
-        arm_x.move(distances[2])
-        arm_z.move(distances[1])
+        self.__move(distances)
         generator.stop()
-
-        arm_z.move(distances[1])
-        arm_x.move(distances[2])
-        tower.move(distances[0])
+        self.__move_back(distances)
