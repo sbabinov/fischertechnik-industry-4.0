e2aef29a31a9bd7e89eae0ad77f8e13c4914eb4a
Serafim Babinov <serafimbabinov106@gmail.com>
2025-04-06 16:21:00 +0300

    fix: solve some problems with motors

--- src/stages/crane.py
+++ src/stages/crane.py
@@ -8,6 +8,7 @@ class Crane(Stage):
         self.tower = Motor(self._stage, 1)
         self.arm_z = Motor(self._stage, 2)
         self.arm_x = Motor(self._stage, 3)
+        self.generator = Motor(self._stage, 4)
 
     def __move(self, distances: Tuple) -> None:
         self.tower.move(distances[0])
@@ -17,21 +18,20 @@ class Crane(Stage):
     def __moveBack(self, distances: Tuple) -> None:
         self.arm_z.move(-distances[1])
         self.arm_x.move(-distances[2])
-        self.tower.move(-distances[0])
+        self.tower.move(-distances[0] )
 
     def calibrate(self) -> None:
         """ Calibrates all crane motors. """
-        for motor_id in 1, 2, 3:
+        for motor_id in 2, 3, 1:
             Motor(self._stage, motor_id).calibrate()
         Motor(self._stage, 1).move(-925)
 
     def takeFromStorage(self) -> None:
         """ The crane takes a cargo from the storage and returns to the calibrated position. """
         distances = (-475, -250, -195)
-        generator = Motor(self._stage, 4)
 
         self.__move(distances)
-        generator.move(wait=False)
+        self.generator.move(wait=False)
         out = self._stage.output(8)
         out.setLevel(512)
         time.sleep(2)
@@ -40,15 +40,14 @@ class Crane(Stage):
     def takeFromSortingCenter(self, color) -> None:
         """ The crane takes a cargo from the sorting center and returns to the calibrated position. """
         if color == Cargo.WHITE:
-            distances = (465, -855, -375)
+            distances = (465, -860, -375)
         elif color == Cargo.RED:
-            distances = (545, -855, -420)
+            distances = (545, -860, -420)
         else:
-            distances = (615, -855, -600)
-        generator = Motor(self._stage, 4)
+            distances = (615, -860, -600)
 
         self.__move(distances)
-        generator.move(wait=False)
+        self.generator.move(wait=False)
         out = self._stage.output(8)
         out.setLevel(512)
         time.sleep(2)
@@ -57,17 +56,15 @@ class Crane(Stage):
     def putInStorage(self) -> None:
         """ The crane puts a cargo in the storage and returns to the calibrated position. """
         distances = (-475, -250, -195)
-        generator = Motor(self._stage, 4)
 
         self.__move(distances)
-        generator.stop()
+        self.generator.stop()
         self.__moveBack(distances)
 
     def putInPaintingCenter(self) -> None:
         """ The crane puts a cargo in the painting center and returns to the calibrated position. """
-        distances = (5, -600, -835)
-        generator = Motor(self._stage, 4)
+        distances = (5, -600, -870)
 
         self.__move(distances)
-        generator.stop()
+        self.generator.stop()
         self.__moveBack(distances)
