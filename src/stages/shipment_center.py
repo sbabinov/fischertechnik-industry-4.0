74b1de78327bf638dc7e7d9eeeda672a21f0f912
MaxArt <maxartemev@mail.ru>
2025-04-07 15:23:29 +0300

    feat: add new class ShipmentCenter

--- src/stages/shipment_center.py
+++ src/stages/shipment_center.py
@@ -1,4 +1,14 @@
 from .stage import Stage
 
+import time
+
 class ShipmentCenter(Stage):
-    ...
+    def __init__(self, host: str):
+        super().__init__(host)
+        self._init_components()
+
+    def _init_components(self):
+        self.motor_stand = Motor(self, 1)
+
+        self.polishing = self.output(3) #полировка
+        self.tape = self.output(6) #лента
