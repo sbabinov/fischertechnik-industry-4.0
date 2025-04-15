4366cab350c4863a6d55d377817e4485206f736a
MaxArt <maxartemev@mail.ru>
2025-04-15 11:14:50 +0300

    refactor: ShipmentCenter

--- src/stages/shipment_center.py
+++ src/stages/shipment_center.py
@@ -48,46 +48,51 @@ class ShipmentCenter(Stage):
                 break
 
     def stand(self):
-        while self.buttonCrane.value() != 15000 and self.sensorCheck.sensorCheck:
+        #while self.buttonCrane.value() != 15000 and self.sensorCheck.sensorCheck:
 
-            self.motorStand.move(100, 300, False)
-            while not self.motorStand.isFinished():
-                self._stage.updateWait()
-                if self.buttonStandMinus.value() != 15000:
-                    self.motorStand.stop()
-                    break
+        self.motorStand.move(100, 300, False)
+        while not self.motorStand.isFinished():
+            self._stage.updateWait()
+            if self.buttonStandMinus.value() != 15000:
+                self.motorStand.stop()
+                break
 
-            self.motorStand.move(100, -300, False)
-            while not self.motorStand.isFinished():
-                self._stage.updateWait()
-                if self.buttonStandDown.value() != 15000:
-                    self.motorStand.stop()
-                    self.polishing.setLevel(512)
-                    time.sleep(3)
-                    break
-            self.polishing.setLevel(0)
+        self.motorStand.move(100, -300, False)
+        while not self.motorStand.isFinished():
+            self._stage.updateWait()
+            if self.buttonStandDown.value() != 15000:
+                self.motorStand.stop()
+                self.polishing.setLevel(512)
+                time.sleep(3)
+                break
+        self.polishing.setLevel(0)
 
-            self.motorStand.move(100, -300, False)
-            while not self.motorStand.isFinished():
-                self._stage.updateWait()
-                if self.buttonStandPlus.value() != 15000:
-                    self.motorStand.stop()
-                    self.compressor.setLevel(512)
-                    self.throwOut.setLevel(512)
-                    time.sleep(0.5)
-                    break
+        self.motorStand.move(100, -300, False)
+        while not self.motorStand.isFinished():
+            self._stage.updateWait()
+            if self.buttonStandPlus.value() != 15000:
+                self.motorStand.stop()
+                self.compressor.setLevel(512)
+                self.throwOut.setLevel(512)
+                time.sleep(0.5)
+                break
 
-            self.throwOut.setLevel(0)
-            self.compressor.setLevel(0)
-            self.calibrate()
-            self.tape.setLevel(512)
-            while self.sensorTape.value() != 15000:
-                pass
-            time.sleep(2)
-            self.tape.setLevel(0)
-            self.sensorCheck.sensorCheck = False
+        self.throwOut.setLevel(0)
+        self.compressor.setLevel(0)
+        self.motorStand.move(100, 300, False)
+        self.tape.setLevel(512)
+        while self.sensorTape.value() != 15000:
+            pass
+        time.sleep(2)
+        self.tape.setLevel(0)
+        while not self.motorStand.isFinished():
+            self._stage.updateWait()
+            if self.buttonStandMinus.value() != 15000:
+                self.motorStand.stop()
+                break
+        self.sensorCheck.sensorCheck = False #не нужен
 
-    def runShipmentCenter(self):
+    def run(self):
         self._isRunning = True
         self.stand()
         self._isRunning = False
