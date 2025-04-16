28159fc3feb04a37e7e02b4ab9e40d6c2b424199
kirillnamestnikov <kir.namestnikov@gmail.com>
2025-04-16 16:46:37 +0300

    feat: add single cargo and all cargos routes

--- src/server/server.py
+++ src/server/server.py
@@ -1,11 +1,49 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+from typing import List
+from src.stages.stage import Cargo
+from src.factory import Factory
+
+factory = Factory()
+
+class CargoResponse(BaseModel):
+    row: int
+    col: int
+    state: Cargo
+
+
+class CargoListResponse(BaseModel):
+    storage: List[CargoResponse] 
+    
 
 app = FastAPI()
 
 @app.get("/")
-def main():
+async def main():
     return {"ok": "Hello world"}
+
+
+@app.get("/storage", response_model=CargoListResponse)
+async def getAllCargos():
+    cargos = []
+    for row in range(3):
+        for col in range(3):
+            cargo = factory.getStorage(row, col)
+            cargos.append({
+                "row": row,
+                "col": col,
+                "cargo": cargo
+                })
+    return {"storage": cargos}
+
+
+@app.get("/storage/{row}/{col}")
+async def getSingleCargo(row: int, col: int):
+    cargo = factory.getStorage(row, col)
+    return {"row": row,
+            "col": col,
+            "cargo": cargo}
 
 
 if __name__ == "__main__":
