07e10cfdce7c3b2d67646110a104e930dc83ba75
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 14:25:49 +0300

    feat: add python manager class declaration

--- src/qt_app/pythonmanager.h
+++ src/qt_app/pythonmanager.h
@@ -1,15 +1,33 @@
 #ifndef PYTHONMANAGER_H
 #define PYTHONMANAGER_H
 
+#include <QProcess>
 #include <QObject>
 
 class PythonManager : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit PythonManager(QObject *parent = nullptr);
+    explicit PythonManager(QObject* parent = nullptr);
+
+    Q_INVOKABLE void startPythonScript(const QString& scriptPath);
+    Q_INVOKABLE void stopPythonScript();
+    Q_INVOKABLE void sendCommand(const QString& command);
 
 signals:
+    void pythonOutputReceived(const QString& output);
+    void pythonErrorReceived(const QString& error);
+    void pythonFinished();
+
+private slots:
+    void onPythonOutput();
+
+    void onPythonError();
+
+    void onPythonFinished();
+private:
+    QProcess m_process;
 };
 
-#endif // PYTHONMANAGER_H
+#endif
