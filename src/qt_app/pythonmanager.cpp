3d27bc0dfbb47832a138c61c8ba26138cf746923
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 14:30:17 +0300

    feat: add definition of python manager constructor and methods

--- src/qt_app/pythonmanager.cpp
+++ src/qt_app/pythonmanager.cpp
@@ -1,5 +1,32 @@
 #include "pythonmanager.h"
 
-PythonManager::PythonManager(QObject *parent)
-    : QObject{parent}
-{}
+PythonManager::PythonManager(QObject* parent) : QObject(parent)
+{
+    connect(&m_process, &QProcess::readyReadStandardOutput, this, &PythonManager::onPythonOutput);
+    connect(&m_process, &QProcess::readyReadStandardError, this, &PythonManager::onPythonError);
+    connect(&m_process, QOverload< int, QProcess::ExitStatus >::of(&QProcess::finished), this, &PythonManager::onPythonFinished);
+}
+
+void PythonManager::startPythonScript(const QString& scriptPath)
+{
+    if (m_process.state() == QProcess::NotRunning)
+    {
+        m_process.start("python", QStringList() << scriptPath);
+    }
+}
+
+void PythonManager::stopPythonScript()
+{
+    if (m_process.state() != QProcess::NotRunning)
+    {
+        m_process.terminate();
+    }
+}
+
+void PythonManager::sendCommand(const QString& command)
+{
+    if (m_process.state() == QProcess::Running)
+    {
+        m_process.write((command + "\n").toUtf8());
+    }
+}
