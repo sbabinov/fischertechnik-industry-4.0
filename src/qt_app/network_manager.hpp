d7ce820159aca26cdf77ea247559c416fad6c3c0
Dmitriy <Dmitry5335@yandex.ru>
2025-05-18 02:01:35 +0300

    design: change spacing in network manager

--- src/qt_app/network_manager.hpp
+++ src/qt_app/network_manager.hpp
@@ -7,25 +7,25 @@
 
 class NetworkManager : public QObject
 {
-    Q_OBJECT
-    Q_PROPERTY(QString response READ response NOTIFY responseChanged)
+  Q_OBJECT
+  Q_PROPERTY(QString response READ response NOTIFY responseChanged)
 
 public:
-    explicit NetworkManager(QObject* parent = nullptr);
-    QString response() const;
+  explicit NetworkManager(QObject* parent = nullptr);
+  QString response() const;
 
-    Q_INVOKABLE void getRequest(const QString &url);
-    Q_INVOKABLE void postRequest(const QString &url, const QString &jsonData);
+  Q_INVOKABLE void getRequest(const QString &url);
+  Q_INVOKABLE void postRequest(const QString &url, const QString &jsonData);
 
 signals:
-    void responseChanged(const QString &response);
-    void errorOccurred(const QString &error);
+  void responseChanged(const QString &response);
+  void errorOccurred(const QString &error);
 
 private slots:
-    void onReplyFinished(QNetworkReply *reply);
+  void onReplyFinished(QNetworkReply *reply);
 private:
-    QNetworkAccessManager *manager_;
-    QString response_;
+  QNetworkAccessManager *manager_;
+  QString response_;
 };
 
 #endif
