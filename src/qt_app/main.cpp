ec20c1112bbd8eba4ac7420907f923e9806853c3
Dmitriy <Dmitry5335@yandex.ru>
2025-05-17 22:02:21 +0300

    feat: add network manager to main

--- src/qt_app/main.cpp
+++ src/qt_app/main.cpp
@@ -1,7 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include "pythonmanager.h"
+#include "network_manager.hpp"
 
 int main(int argc, char *argv[])
 {
