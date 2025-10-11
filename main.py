447aec387020b564d795f70ec832220eea49c914
Dmitriy <Dmitry5335@yandex.ru>
2025-10-11 23:47:53 +0300

    [REFACTOR] Move starting app to main

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import uvicorn
+from src import Config, Factory, create_app
+
 if __name__ == "__main__":
-    print("OK")
+    config = Config.from_env()
+    factory = Factory({
+        'storage_ip': config.storage_ip,
+        'crane_ip': config.crane_ip,
+        'sort_center_ip': config.sort_center_ip,
+        'shipment_center_ip': config.shipment_center_ip,
+        'paint_center_ip': config.paint_center_ip
+    })
+
+    app, host, port = create_app(factory, "192.168.1.41", 8000)
+    uvicorn.run(app, host=host, port=port)
