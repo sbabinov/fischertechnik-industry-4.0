e6a0ec34508b718100ad0dd02b57feaa41948f81
Dmitriy <Dmitry5335@yandex.ru>
2025-10-11 23:49:11 +0300

    [FEAT] Add init for server

---
+++ src/api/__init__.py
@@ -0,0 +1,7 @@
+from .config import Config
+from .server import create_app
+
+__all__ = [
+  'Config',
+  'create_app'
+]
--- src/api/server.py
+++ src/api/server.py
@@ -1,93 +1,66 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
-
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent))
-
-from factory import Factory
-from stages.stage import Cargo
-import uvicorn
 import asyncio
-from config import Config
+from ..models.models import CargoListRequest, CoordsListRequest
 
-class CargoListRequest(BaseModel):
-    cargos: List[List[Cargo]]
+def create_app(factory, host: str = "0.0.0.0", port: int = 8000):
+    app = FastAPI()
+    task_queue = asyncio.Queue()
+    stop_flag = False
 
-class CoordsListRequest(BaseModel):
-    coords: List[List[int]]
+    async def async_worker():
+        while not stop_flag:
+            try:
+                task_func, kwargs = await task_queue.get()
+                print(f"STATUS:\t  Starting task { task_func.__name__ }")
+                await task_func(**kwargs)
+                print(f"STATUS:\t  Finished task { task_func.__name__ }")
+                task_queue.task_done()
+            except Exception as e:
+                print(f"STATUS:\t  Task failed: {e}")
 
-config = Config.from_env()
-factory = Factory({
-    'storage_ip': config.storage_ip,
-    'crane_ip': config.crane_ip,
-    'sort_center_ip': config.sort_center_ip,
-    'shipment_center_ip': config.shipment_center_ip,
-    'paint_center_ip': config.paint_center_ip
-})
+    @app.on_event("startup")
+    async def startup_event():
+        asyncio.create_task(async_worker())
 
-task_queue = asyncio.Queue()
-stop_flag = False
+    @app.on_event("shutdown")
+    async def shutdown_event():
+        global stop_flag
+        stop_flag = True
+        await task_queue.join()
 
-async def async_worker():
-    while not stop_flag:
-        try:
-            task_func, kwargs = await task_queue.get()
-            print(f"STATUS:\t  Starting task { task_func.__name__ }")
-            await task_func(**kwargs)
-            print(f"STATUS:\t  Finished task { task_func.__name__ }")
-            task_queue.task_done()
-        except Exception as e:
-            print(f"STATUS:\t  Task failed: {e}")
+    @app.post("/write_storage")
+    async def run_task1(request: CargoListRequest):
+        await task_queue.put((factory.write_storage, { "new_storage": request.cargos }))
+        return { "status": "queued", "task": "write_storage"}
 
-app = FastAPI()
+    @app.post("/process_cargos")
+    async def run_task2(request: CoordsListRequest):
+        await task_queue.put((factory.process_cargos, { "coords": request.coords }))
+        return { "status": "queued", "task": "process_cargos" }
 
-@app.on_event("startup")
-async def startup_event():
-    asyncio.create_task(async_worker())
+    @app.post("/return_cargos")
+    async def run_task3(request: CargoListRequest):
+        await task_queue.put((factory.return_cargos, { "new_storage": request.cargos }))
+        return { "status": "queued", "task": "return_cargos" }
 
-@app.on_event("shutdown")
-async def shutdown_event():
-    global stop_flag
-    stop_flag = True
-    await task_queue.join()
+    @app.post("/sort_cargos")
+    async def run_task4(request: CargoListRequest):
+        await task_queue.put((factory.sort_cargos, { "new_storage": request.cargos }))
+        return { "status": "queued", "task": "sort_cargos" }
 
-@app.post("/write_storage")
-async def run_task1(request: CargoListRequest):
-    await task_queue.put((factory.write_storage, { "new_storage": request.cargos }))
-    return { "status": "queued", "task": "write_storage"}
+    @app.get("/get_storage")
+    def get_storage():
+        return factory.get_storage()
 
-@app.post("/process_cargos")
-async def run_task2(request: CoordsListRequest):
-    await task_queue.put((factory.process_cargos, { "coords": request.coords }))
-    return { "status": "queued", "task": "process_cargos" }
+    @app.get("/get_status")
+    def get_status():
+        return factory.get_status()
 
-@app.post("/return_cargos")
-async def run_task3(request: CargoListRequest):
-    await task_queue.put((factory.return_cargos, { "new_storage": request.cargos }))
-    return { "status": "queued", "task": "return_cargos" }
+    @app.get("/queue_status")
+    async def queue_status():
+        return {
+            "queue_size": task_queue.qsize(),
+            "tasks_in_progress": task_queue._unfinished_tasks
+        }
 
-@app.post("/sort_cargos")
-async def run_task4(request: CargoListRequest):
-    await task_queue.put((factory.sort_cargos, { "new_storage": request.cargos }))
-    return { "status": "queued", "task": "sort_cargos" }
-
-@app.get("/get_storage")
-def get_storage():
-    return factory.get_storage()
-
-@app.get("/get_status")
-def get_status():
-    return factory.get_status()
-
-@app.get("/queue_status")
-async def queue_status():
-    return {
-        "queue_size": task_queue.qsize(),
-        "tasks_in_progress": task_queue._unfinished_tasks
-    }
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="192.168.1.41", port=8000)
+    return app, host, port
