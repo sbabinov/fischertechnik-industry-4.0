c9d09442ed3b3f57543495121a7efc8a3b81e903
Dmitriy <Dmitry5335@yandex.ru>
2025-10-12 17:17:22 +0300

    [TEST] Add test for models

---
+++ tests/test_models.py
@@ -0,0 +1,32 @@
+import pytest
+from ..src.models import CargoListRequest, CoordsListRequest
+from ..src.core.stages.stage import Cargo
+
+class TestModels:
+    def test_cargo_list_request_valid(self):
+        data = {
+            "cargos": [
+                [Cargo.WHITE, Cargo.BLUE, Cargo.RED],
+                [Cargo.EMPTY, Cargo.UNDEFINED, Cargo.WHITE]
+            ]
+        }
+        request = CargoListRequest(**data)
+        assert request.cargos == data["cargos"]
+
+    def test_cargo_list_request_invalid(self):
+        with pytest.raises(ValueError):
+            CargoListRequest(cargos="invalid")
+
+    def test_coords_list_request_valid(self):
+        data = {"coords": [[0, 0], [1, 1], [2, 2]]}
+        request = CoordsListRequest(**data)
+        assert request.coords == data["coords"]
+
+    def test_coords_list_request_invalid_coords(self):
+        with pytest.raises(ValueError):
+            CoordsListRequest(coords=[[0, 0, 0]])
+
+    def test_coords_list_request_empty(self):
+        data = {"coords": []}
+        request = CoordsListRequest(**data)
+        assert request.coords == []
