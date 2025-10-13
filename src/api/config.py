5865702961020b61c6428d73ec0390c97dd5f11f
Dmitriy <Dmitry5335@yandex.ru>
2025-10-14 00:17:35 +0300

    [FEAT] Add new env

--- src/api/config.py
+++ src/api/config.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
+    factory_mode: str
+    server_host: str
+    server_port: str
     storage_ip: str
     crane_ip: str
     sort_center_ip: str
@@ -12,6 +15,9 @@ class Config:
     @classmethod
     def from_env(cls):
         return cls(
+            factory_mode=os.getenv('FACTORY_MODE', 'mock'),
+            server_host=os.getenv('SERVER_HOST', '127.0.0.1'),
+            server_port=os.getenv('SERVER_PORT', '8080'),
             storage_ip=os.getenv('STORAGE_IP', '192.168.1.10'),
             crane_ip=os.getenv('CRANE_IP', '192.168.1.11'),
             sort_center_ip=os.getenv('SORT_CENTER_IP', '192.168.1.12'),
