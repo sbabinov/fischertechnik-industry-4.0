2a225067d376ca93a83b0b880c2927e17510e737
Dmitriy <Dmitry5335@yandex.ru>
2025-10-13 23:54:50 +0300

    [TEST] Replace mock by mock_factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,15 @@
 import pytest
-from unittest.mock import Mock, AsyncMock
 from fastapi.testclient import TestClient
+from src import MockFactory
 
 @pytest.fixture
 def mock_factory():
-    factory = Mock()
-    factory.write_storage = AsyncMock()
-    factory.process_cargos = AsyncMock()
-    factory.return_cargos = AsyncMock()
-    factory.sort_cargos = AsyncMock()
-    factory.get_storage = Mock(return_value=[[1, 1, 1], [2, 2, 2], [3, 3, 3]])
-    factory.get_status = Mock(return_value={
-        "storage": "Ожидаю",
-        "crane": "Ожидаю", 
-        "handle_center": "Ожидаю",
-        "sort_center": "Ожидаю"
-    })
+    factory = MockFactory({})
     return factory
 
 @pytest.fixture
 def test_client(mock_factory):
-    from ..src.api.server import create_app
+    from src import create_app
     app, _, _ = create_app(mock_factory)
     return TestClient(app)
 
